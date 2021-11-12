3d73d9dd4324ff4a5ff7dd107bd66122ad965138
Fabiano Parente <fabiano@nunes.me>
2021-11-12 00:40:05 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+	extends: "stylelint-config-standard-scss",
+	plugins: ["stylelint-no-unsupported-browser-features"],
+	rules: {
+		"max-nesting-depth": 2,
+		"selector-class-pattern": ".*",
+		"selector-max-specificity": "0,2,2",
+		"selector-max-compound-selectors": 2,
+		"plugin/no-unsupported-browser-features": [
+			true,
+			{
+				ignorePartialSupport: true,
+			},
+		],
+	},
+};

610551ff9c1e852fcbcf3fedcfd6fb2bfddb86a8
Fabiano Parente <fabiano@nunes.me>
2021-11-12 23:37:57 -0300

    implementa eslint e prettier

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = {
-	extends: "stylelint-config-standard-scss",
-	plugins: ["stylelint-no-unsupported-browser-features"],
+	extends: 'stylelint-config-standard-scss',
+	plugins: ['stylelint-no-unsupported-browser-features'],
 	rules: {
-		"max-nesting-depth": 2,
-		"selector-class-pattern": ".*",
-		"selector-max-specificity": "0,2,2",
-		"selector-max-compound-selectors": 2,
-		"plugin/no-unsupported-browser-features": [
+		'max-nesting-depth': 2,
+		'selector-class-pattern': '.*',
+		'selector-max-specificity': '0,2,2',
+		'selector-max-compound-selectors': 2,
+		'plugin/no-unsupported-browser-features': [
 			true,
 			{
 				ignorePartialSupport: true,
 			},
 		],
 	},
-};
+}
